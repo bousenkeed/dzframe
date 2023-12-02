70b061750dceaa14dcf55a12286465538aacee43
bousenkeed <ant.sakh38@gmail.com>
2023-12-03 02:04:06 +0300

    dz

---
+++ script.js
@@ -0,0 +1,45 @@
+const TodoItemNewTest = {
+    props: ['todo'],
+    template: `<li>{{ todo }}</li>`
+}
+
+
+const watchExample = Vue.createApp({
+    data() {
+        return {
+            watchInput: '',
+            groceryList: [
+                'Погулять с собакой',
+                'Приготовить обед',
+                'Заварить кофе'
+            ]
+        }
+    },
+    watch: {
+        watchInput(inputValue) {
+            this.watchInput = inputValue;
+        }
+    },
+    methods: {
+
+        getAnswer() {
+            axios
+            .get('https://jsonplaceholder.typicode.com/todos/4')
+            .then(response => console.log(response.data))
+            .catch(error => error)
+        },
+    addTodo() {
+        this.groceryList.push(this.watchInput)
+    },
+    deleteItem() {
+        this.groceryList.splice(this.groceryList.length - 1 , 1);
+    },
+    sortList() {
+        this.groceryList.sort();
+    }
+},
+components: {
+    TodoItemNewTest
+}
+
+}).mount('#example');
